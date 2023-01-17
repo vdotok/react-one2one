f94f5f5f70b123835c8c478b5f8b4c432d40d2ad
asadmahmood <asad.mahmood@norgic.com>
2023-01-17 15:45:37 +0500

    new build with project id 115G1WZI

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export const BASE_URL = "https://q-tenant.vdotok.dev/API/v0/";
 //export const PROJECT_ID = "6NE92I";
-// export const PROJECT_ID = "115G1WZI";
- export const PROJECT_ID = "407IFV5M";
+export const PROJECT_ID = "115G1WZI";
+// export const PROJECT_ID = "407IFV5M";
